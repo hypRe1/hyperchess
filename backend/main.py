38d45707cf04e16b8a1c2c0590e978f904b2c2b6
hypRe1 <james.fenwick@mac.com>
2024-02-24 19:02:10 +0000

    Database update

--- backend/main.py
+++ backend/main.py
@@ -16,13 +16,11 @@ async def lifespan(_: FastAPI):
     await FastAPILimiter.init(redis_connection)
 
     async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
     yield
     await FastAPILimiter.close()
-
-    # async with engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.drop_all)
 
 
 app = FastAPI(
--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,12 @@
 from database import Base
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, Table
 from sqlalchemy.dialects.postgresql import BYTEA
 
 
 class Users(Base):
-    __tablename__ = "Users"
+    __tablename__ = "users"
 
-    username = Column(String(length=255), primary_key=True)
+    username = Column(String(length=32), primary_key=True)
     email = Column(String(length=255), unique=True)
     about_me = Column(String(length=500))
     password = Column(String(length=255))
@@ -19,9 +19,60 @@ class Users(Base):
 
 
 class Matches(Base):
-    __tablename__ = "Matches"
+    __tablename__ = "matches"
 
-    match_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     white = Column(String(length=255))
     black = Column(String(length=255))
     moves = Column(BYTEA)
+
+
+class Mistakes(Base):
+    __tablename__ = "mistakes"
+
+    id = Column(Integer, primary_key=True)
+    fen = Column(String(length=127))
+    moves = Column(BYTEA)
+    rating = Column(Integer)
+
+
+class Puzzles(Base):
+    __tablename__ = "puzzles"
+
+    id = Column(Integer, primary_key=True)
+    fen = Column(String(length=127))
+    moves = Column(BYTEA)
+    rating = Column(Integer)
+
+
+class ThemeTags(Base):
+    __tablename__ = "themes"
+
+    id = Column(Integer, primary_key=True)
+    tag_title = Column(String(length=255))
+
+
+class Openings(Base):
+    __tablename__ = "openings"
+
+    id = Column(Integer, primary_key=True)
+    eco = Column(BYTEA)
+    name = Column(String(length=127))
+    moves = Column(BYTEA)
+
+
+PuzzleTheme = Table(
+    "puzzle_theme",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("puzzle_id", Integer, ForeignKey("puzzles.id")),
+    Column("theme_id", Integer, ForeignKey("themes.id")),
+)
+
+PuzzleOpening = Table(
+    "puzzle_opening",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("puzzle_id", Integer, ForeignKey("puzzles.id")),
+    Column("opening_id", Integer, ForeignKey("openings.id")),
+)

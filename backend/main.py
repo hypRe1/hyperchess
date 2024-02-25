aa34f5c7e23d554dc4ee27dec96032bfb2ebacc4
hypRe1 <james.fenwick@mac.com>
2024-02-25 17:09:19 +0000

    small changes

--- backend/main.py
+++ backend/main.py
@@ -42,4 +42,4 @@ app.add_middleware(CORSMiddleware, allow_origins=origins)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
+    uvicorn.run("main:app", reload=True)

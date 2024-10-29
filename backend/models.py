10f107300b055cd9556784f72850d84096c67fc9
hypRe1 <james.fenwick@mac.com>
2024-10-29 12:09:12 +0000

    update default theme

--- backend/models.py
+++ backend/models.py
@@ -34,7 +34,7 @@ class Appearance(Base):
     username = mapped_column(ForeignKey("users.username"), primary_key=True)
     board = Column(String(length=32), nullable=False, default="blue")
     piece = Column(String(length=32), nullable=False, default="staunty")
-    theme = Column(String(length=32), nullable=False, default="skeleton")
+    theme = Column(String(length=32), nullable=False, default="crimson")
     dark = Column(Boolean, nullable=False, default=True)
     users: Mapped["Users"] = relationship(back_populates="appearance")
 
